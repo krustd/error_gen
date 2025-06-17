cb234020874204a98826139699ed9ee67002c319
krust <yzk_study@163.com>
2025-06-17 18:49:12 +0800

    Add auto for quickly gen Error code

--- main.go
+++ main.go
@@ -49,7 +49,6 @@ func main() {
 			if importPath == "" {
 				importPath = modelName + "/errorcode"
 			}
-			// print(pbFile, modelName, importPath)
 			cmd.RunGenerate(modelName, pbFile, importPath)
 		},
 	}
@@ -58,7 +57,25 @@ func main() {
 	genCmd.Flags().StringP("pbfile", "p", "", ".pb.go 文件路径（可选，默认 ./errorcode/error.pb.go）")
 	genCmd.Flags().StringP("importpath", "i", "", ".pb.go文件所属的包名（可选，默认 当前目录所属报名/errorcode）")
 
-	rootCmd.AddCommand(createCmd, buildCmd, genCmd)
+	var auto = &cobra.Command{
+		Use:   "auto",
+		Short: "全流程共同进行",
+		Run: func(c *cobra.Command, args []string) {
+			path, _ := c.Flags().GetString("path")
+			if path == "" {
+				path = "./error.proto"
+			}
+			cmd.RunBuild(path)
+			modelName, _ := c.Flags().GetString("modelname")
+			pbFile := "./errorcode/error.pb.go"
+			importPath := modelName + "/errorcode"
+			cmd.RunGenerate(modelName, pbFile, importPath)
+		},
+	}
+	auto.Flags().StringP("path", "p", "", "proto 文件路径（可选，默认 ./error.proto）")
+	auto.Flags().StringP("modelname", "m", "", "当前目录所属包名")
+	auto.MarkFlagRequired("modelname")
+	rootCmd.AddCommand(createCmd, buildCmd, genCmd, auto)
 
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
